2c4069fd92a85c02cc68e8f1cab82c4c39073ad9
natively.dev <api@natively.dev>
2026-03-10 13:40:50 +0000

    revert: Restored to commit 7d71affd - chore: Validate babel.config.js worklets plugin order

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
+
 module.exports = function (api) {
   api.cache(true);
+
+  // Ensure NODE_ENV is set for builds
+  if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'production';
+  }
 
   const EDITABLE_COMPONENTS =
     process.env.EXPO_PUBLIC_ENABLE_EDIT_MODE === "TRUE" &&
@@ -41,7 +47,8 @@ module.exports = function (api) {
       ],
       ...EDITABLE_COMPONENTS,
       "@babel/plugin-proposal-export-namespace-from",
-      "react-native-worklets/plugin", // react-native-worklets/plugin must be listed last!
+      "react-native-reanimated/plugin",
+      "react-native-worklets/plugin", // CRITICAL: react-native-worklets/plugin MUST be listed last!
     ],
   };
 };
